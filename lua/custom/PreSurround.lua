ae4f513e20d64f1ffda534c09d5abc8ac2515b1a
Ryan Mash <ryanamash@gmail.com>
2025-04-13 16:58:47 -0400

    Rework PreSurround to use extmarks and perfect nesting

--- lua/custom/PreSurround.lua
+++ lua/custom/PreSurround.lua
@@ -3,33 +3,42 @@
 -------------------------------------------------------------------------------
 require('custom.utils.MakeSurround')
 
-function PreSurround(line, buf_id, n_buf_id, win_id, n_win_id, ns_id, ext_mark)
-	local text = vim.api.nvim_buf_get_text(n_buf_id, 0, 0, -1, -1, {})
-	local ext_mark_pos = vim.api.nvim_buf_get_extmark_by_id(n_buf_id, ns_id, ext_mark, {})
+local surround = vim.api.nvim_create_namespace('surround_ops')
+vim.api.nvim_set_hl(surround, 'NormalFloat', { ctermbg = 'none', })
+vim.api.nvim_set_hl(surround, 'FloatBorder', { ctermbg = 'none', })
+
+function PreSurround(n_buf_id, win_id, n_win_id)
+	local ns_mrk_pos = vim.api.nvim_buf_get_extmark_by_id(n_buf_id, surround, 1, {})
+	local ne_mrk_pos = vim.api.nvim_buf_get_extmark_by_id(n_buf_id, surround, 2, {})
+	local text
 	local pos
-	local buf_ext_mark
 	local col
+
+	col = vim.fn.charcol('$', win_id) - vim.fn.charcol('.', win_id)
+	if col > 1 then
+		text = vim.api.nvim_buf_get_text(n_buf_id, ns_mrk_pos[1], ns_mrk_pos[2] - 1, ne_mrk_pos[1], ne_mrk_pos[2], {})
+	else
+		text = vim.api.nvim_buf_get_text(n_buf_id, ns_mrk_pos[1], ns_mrk_pos[2], ne_mrk_pos[1], ne_mrk_pos[2] + 1, {})
+	end
 
 	vim.api.nvim_win_close(n_win_id, false)
 	vim.api.nvim_set_current_win(win_id)
---	print(n_buf_id, ext_mark_pos[1], ext_mark_pos[2], ns_id, ext_mark)
 
-	col = vim.fn.charcol('$') - vim.fn.charcol('.')
+	pos = vim.api.nvim_win_get_cursor(win_id)
+	local indent = vim.fn.indent(pos[1] - 1)
 
-	vim.api.nvim_buf_set_lines(buf_id, line - 1, line, true, {''})
+	if col == 0 then
+		text[1] = vim.text.indent(indent, text[1])
+	end
+
+	for i = 2, #text, 1 do
+		text[i] = vim.text.indent(indent, text[i])
+	end
 	vim.api.nvim_put(text, 'c', true, true)
 
 	if col > 1 then
 		vim.api.nvim_feedkeys('h', 'n', true)
 	end
-
-	pos = vim.api.nvim_win_get_cursor(win_id)
-	buf_ext_mark = vim.api.nvim_buf_set_extmark(buf_id, ns_id, pos[1] - 1, ext_mark_pos[2], {})
-
-	vim.cmd('normal! ='..(#text - 1)..'k\']')
-
-	ext_mark_pos = vim.api.nvim_buf_get_extmark_by_id(buf_id, ns_id, buf_ext_mark, {})
-	vim.api.nvim_win_set_cursor(win_id, {ext_mark_pos[1] + 1, ext_mark_pos[2]})
 
 	vim.api.nvim_feedkeys('a', 'n', true)
 
@@ -55,14 +64,10 @@ local function win_resize()
 	end
 end
 
-local ns_id = vim.api.nvim_create_namespace('surround_insert')
-vim.api.nvim_set_hl(ns_id, 'NormalFloat', { ctermbg = 'none', })
-vim.api.nvim_set_hl(ns_id, 'FloatBorder', { ctermbg = 'none', })
-
 vim.api.nvim_create_user_command('PreSurround',
 function (opts)
 	local s_chars = MakeSurround(opts.args)
-	local put_chars = {s_chars[1]..s_chars[2]}
+	local put_chars = s_chars[1]..s_chars[2]
 	local filetype = vim.bo.filetype
 	local buf_id = vim.fn.bufnr()
 	local n_buf_id = vim.api.nvim_create_buf(false, true)
@@ -70,8 +75,8 @@ function (opts)
 	local n_win_id
 
 	local au_opts = {
-		group = 'surround_insert',
-		buffer = 0,
+		group = 'presurround',
+		buffer = n_buf_id,
 		callback = function () win_resize() end,
 	}
 
@@ -94,8 +99,7 @@ function (opts)
 	local confirm_cmd
 	local quit_cmd
 	local ext_pos
-	local ext_mark
-	local ext_mark_pos
+	local ns_mrk_pos
 	local col
 
 	if win_relative == 'win' then
@@ -106,7 +110,7 @@ function (opts)
 
 	vim.api.nvim_open_win(n_buf_id, true, win_opts)
 	n_win_id = vim.fn.win_getid()
-	vim.api.nvim_win_set_hl_ns(n_win_id, ns_id)
+	vim.api.nvim_win_set_hl_ns(n_win_id, surround)
 
 	vim.api.nvim_put(line, 'c', true, true)
 	vim.api.nvim_win_set_cursor(n_win_id, {1, pos[2]})
@@ -116,24 +120,24 @@ function (opts)
 		vim.api.nvim_feedkeys('h', 'n', true)
 	end
 
-	vim.api.nvim_put(put_chars, 'c', true, true)
+	vim.api.nvim_put({put_chars}, 'c', true, true)
 
 	ext_pos = vim.api.nvim_win_get_cursor(n_win_id)
-	ext_mark = vim.api.nvim_buf_set_extmark(n_buf_id, ns_id, ext_pos[1] - 1, ext_pos[2], {})
+	vim.api.nvim_buf_set_extmark(n_buf_id, surround, ext_pos[1] - 1, ext_pos[2] - #put_chars + 1, { id = 1, right_gravity = false })
+	vim.api.nvim_buf_set_extmark(n_buf_id, surround, ext_pos[1] - 1, ext_pos[2], { id = 2 })
 
 	vim.cmd('normal! ==')
 
-	ext_mark_pos = vim.api.nvim_buf_get_extmark_by_id(n_buf_id, ns_id, ext_mark, {})
-	vim.api.nvim_win_set_cursor(n_win_id, {ext_mark_pos[1] + 1, ext_mark_pos[2]})
---	print(n_buf_id, ext_mark_pos[1], ext_mark_pos[2], ns_id, ext_mark)
+	ns_mrk_pos = vim.api.nvim_buf_get_extmark_by_id(n_buf_id, surround, 1, {})
+	vim.api.nvim_win_set_cursor(n_win_id, {ns_mrk_pos[1] + 1, ns_mrk_pos[2] + #s_chars[1]})
 
 	vim.api.nvim_feedkeys('i', 'n', true)
 
 	vim.bo.filetype = filetype
 
-	confirm_cmd = string.format(':lua PreSurround(%d, %d, %d, %d, %d, %d, %d)<CR>', pos[1], buf_id, n_buf_id, win_id, n_win_id, ns_id, ext_mark)
+	confirm_cmd = string.format(':lua PreSurround(%d, %d, %d)<CR>', n_buf_id, win_id, n_win_id)
 	quit_cmd = string.format(':q | lua vim.api.nvim_set_current_win(%d)<CR>', win_id)
-	vim.api.nvim_create_augroup('surround_insert', { clear = true, })
+	vim.api.nvim_create_augroup('presurround', {})
 	vim.api.nvim_create_autocmd('TextChanged', au_opts)
 	vim.api.nvim_create_autocmd('TextChangedI', au_opts)
 	vim.api.nvim_buf_set_keymap(n_buf_id, 'n', '<Esc>', quit_cmd, {})
