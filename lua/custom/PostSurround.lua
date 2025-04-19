246e4414449d549cdbe23494a3cc055c9bbe8d2c
Ryan Mash <ryanamash@gmail.com>
2025-04-19 13:04:49 -0400

    Fix PostSurround "" bug and put cursor at the end

--- lua/custom/PostSurround.lua
+++ lua/custom/PostSurround.lua
@@ -9,11 +9,14 @@ function PostSurround(chars)
 	local row_start = vim.api.nvim_buf_get_mark(0, "[")[1] - 1
 	local row_end = vim.api.nvim_buf_get_mark(0, "]")[1] - 1
 	local s_chars = MakeSurround(chars)
+	local cur_pos = {row_end + 1, col_end + #s_chars[1] + #s_chars[2] - 1}
 
 	vim.api.nvim_buf_set_text(0, row_end, col_end,
 														row_end, col_end, {s_chars[2]})
 	vim.api.nvim_buf_set_text(0, row_start, col_start,
 														row_start, col_start, {s_chars[1]})
+
+	vim.api.nvim_win_set_cursor(0, cur_pos)
 
 end
 
@@ -25,7 +28,8 @@ local function get_input(mode)
 			['v'] = '`<v`>g@',
 		}
 		if input == nil then return end
-		vim.cmd.let('&operatorfunc = { -> v:lua.PostSurround("'..input..'")}')
+		vim.g.post_surround_input = input
+		vim.o.operatorfunc = '{ -> v:lua.PostSurround(g:post_surround_input) }'
 		vim.fn.feedkeys(keys[mode])
 	end)
 
