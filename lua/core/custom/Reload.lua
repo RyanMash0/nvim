853dd82dc29c9c9437cf5ff0d3123d9e0c659b09
Ryan Mash <rmash@unc.edu>
2026-08-21 12:46:35 -0400

    feat: change LoadLastSession command to Load, allow custom session naming for both Reload and Load commands

--- lua/core/custom/Reload.lua
+++ lua/core/custom/Reload.lua
@@ -1,22 +1,36 @@
 -------------------------------------------------------------------------------
 -- Reload Plugin                                                             --
 -------------------------------------------------------------------------------
-local path = vim.fn.stdpath('data')
-path = vim.fs.joinpath(path, 'sessions')
+local dir = vim.fn.stdpath('data')
+dir = vim.fs.joinpath(dir, 'sessions')
 
-vim.uv.fs_mkdir(path, tonumber('755', 8))
+vim.uv.fs_mkdir(dir, tonumber('755', 8))
 
-path = vim.fs.joinpath(path, 'reload_session.vim')
+local def_path = vim.fs.joinpath(dir, 'reload_session.vim')
 
 vim.api.nvim_create_user_command('Reload',
-function ()
+function (args)
+	local path
+	if args.args == '' then
+		path = def_path
+	else
+		path = vim.fs.joinpath(dir, args.args .. '.vim')
+	end
+
 	vim.cmd('mksession! ' .. path)
 	vim.cmd('restart source ' .. path)
 end,
-{ nargs = 0 })
+{ nargs = '?' })
 
-vim.api.nvim_create_user_command('LoadLastReload',
-function ()
+vim.api.nvim_create_user_command('Load',
+function (args)
+	local path
+	if args.args == '' then
+		path = def_path
+	else
+		path = vim.fs.joinpath(dir, args.args .. '.vim')
+	end
+
 	vim.cmd('source ' .. path)
 end,
-{ nargs = 0 })
+{ nargs = '?' })
